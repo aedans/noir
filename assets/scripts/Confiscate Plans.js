d25f47477594a097e9609288cf3bef1c90bae2c2
aedans <aedan.smith@outlook.com>
2022-04-14 13:30:52 -0700

    Alex is dumb

--- assets/scripts/Confiscate Plans.js
+++ assets/scripts/Confiscate Plans.js
@@ -1,7 +1,7 @@
 // @ts-check
 /** @type {import("../../common/card").CardInfo} */
 exports.card = {
-	text: () => "Additional cost: activate a blue guy. Destroy up to three revealed operations",
+	text: () => "Additional cost: activate a blue agent. Destroy up to three revealed operations",
 	type: () => "operation",
 	colors: () => ["blue"],
 	cost: () => ({ money: 25}),
--- assets/scripts/Flare.js
+++ assets/scripts/Flare.js
@@ -1,7 +1,7 @@
 // @ts-check
 /** @type {import("../../common/card").CardInfo} */
 exports.card = {
-	text: () => "Additional cost: activate a blue guy. Reveal two cards on your opponent's board.",
+	text: () => "Additional cost: activate a blue agent. Reveal two cards on your opponent's board.",
 	type: () => "agent",
 	colors: () => ["blue"],
 	cost: () => ({ money: 10 }),
