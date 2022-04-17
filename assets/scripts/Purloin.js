9eef5b9dd0e5f4ba245f606535bf8bad43005351
aedans <aedan.smith@outlook.com>
2022-04-17 14:21:00 -0700

    Purloin => Subterfuge

--- assets/scripts/Purloin.js
+++ assets/scripts/Purloin.js
@@ -1,30 +1,20 @@
 // @ts-check
 /** @type {import("../../common/card").CardInfo} */
 exports.card = {
-	text: () => "Additional cost: destroy a purple agent in your deck. Destroy a revealed operation and put a hidden copy in your deck.",
+	text: () => "Additional cost: destroy a purple agent in your deck. Steal $10 from your opponent.",
 	type: () => "operation",
 	colors: () => ["purple"],
 	cost: () => ({ money: 10 }),
 	rank: () => 1,
-	playChoice: (util, card, player, opponent) => (cc) => {
-		const destroyTargets = [...opponent.deck, ...opponent.board]
-			.filter(c => util.getCardInfo(c, player, opponent).type(util, c, player, opponent) == "operation")
-			.filter(c => c.revealed == true);
-		return util.chooseTargets(destroyTargets.map(c => c.id), 1, false, (destroy) => {
-			if (destroy == null) return cc(null);
-			return cc({ targets: { destroy } });
-		});
-	},
-	play: (util, card, player, opponent) => {
-		const cards = player.deck
-			.filter(c => util.getCardInfo(c, player, opponent).type(util, c, player, opponent) == "agent")
-			.filter(c => util.getCardInfo(c, player, opponent).colors(util, c, player, opponent).includes("purple"));
-		if (cards.length == 0) return null;
-		return (choice) => {
-			util.destroyRandom(cards, player, opponent);
-			const card = util.getCardState(choice.targets.destroy[0], player, opponent);
-			util.destroy(card.id, player, opponent);
-			player.deck.push({ ...card, revealed: false });
+	play: (util, card, player, opponent) => () => {
+    const cards = player.deck.filter(c => util.getCardInfo(c, player, opponent).type(util, card, player, opponent) == "agent");
+		util.destroyRandom(cards, player, opponent);
+		if (opponent.money < 10) {
+			player.money += opponent.money;
+			opponent.money = 0;
+		} else {
+			opponent.money -= 10;
+			player.money += 10;
 		}
 	}
 }
