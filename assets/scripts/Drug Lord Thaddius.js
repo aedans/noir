556167aa1720a549f6d94328494a7364867a7658
aedans <aedan.smith@outlook.com>
2022-04-16 20:19:30 -0700

    Add the Thadd

--- assets/scripts/Drug Lord Thaddius.js
+++ assets/scripts/Drug Lord Thaddius.js
@@ -1,9 +1,20 @@
 // @ts-check
 /** @type {import("../../common/card").CardInfo} */
 exports.card = {
-	text: () => "Additional cost: activate a purple agent. Whenever a agent in your deck is destroyed on your turn, gain $20.",
+	text: () => "Additional cost: activate a purple agent. Whenever an agent in your deck is destroyed on your turn, gain $20.",
 	type: () => "agent",
 	colors: () => ["purple"],
 	cost: () => ({ money: 50 }),
 	rank: () => 3,
+	effects: {
+		board: (util, card, you, opponent) => (info) => ({
+			...info,
+			destroy: (util, card, player, opponent) => {
+				info.destroy(util, card, player, opponent);
+				if (you.turn) {
+					you.money += 20;
+				}
+			}
+		})
+	}
 }
