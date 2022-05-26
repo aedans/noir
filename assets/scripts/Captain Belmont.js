0295ed9ee0114c8629011c3e2faa476abe6a2aa5
aedans <aedan.smith@outlook.com>
2022-05-25 23:42:13 -0700

    Whoops

--- assets/scripts/Captain Belmont.js
+++ assets/scripts/Captain Belmont.js
@@ -4,16 +4,16 @@ exports.card = {
 	colors: () => ["blue"],
 	cost: () => ({ money: 60 }),
 	rank: () => 2,
-	play: (util, card, player, opponent) => () => card.number.removeed = 0,
-	turn: (util, card, player, opponent) => card.number.removeed = 0,
+	play: (util, card, player, opponent) => () => card.number.removed = 0,
+	turn: (util, card, player, opponent) => card.number.removed = 0,
 	effects: {
 		board: (util, state, you, opponent) => (info) => ({
 			...info,
 			remove: (util, card, player, opponent) => {
-				if (state.number.removeed == 0 && you.turn) {
+				if (state.number.removed == 0 && you.turn) {
 					you.money += 15;
 				}
-				state.number.removeed++;
+				state.number.removed++;
 			},
 		})
 	},
