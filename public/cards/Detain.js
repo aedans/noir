d5f7b47fed6c83584eb477c59c58fc9b3f5f5da1
Alex Hutchinson <alexh747@gmail.com>
2023-01-30 02:57:50 +0000

    two cards ayo

---
+++ public/cards/Detain.js
@@ -0,0 +1,15 @@
+// @ts-check
+/** @type {import("../../common/card").PartialCardInfoComputation} */
+exports.card = (util, game, card) => ({
+    type: "operation",
+    text: "Put an agent on the board back into its owner's deck.",
+    cost: { money: 0, agents: 2 },
+    colors: ["blue"],
+    targets: {
+        types: ["agent"]
+    },
+    play: function* (target){
+        util.bounceCard(game,card, {target: target})
+    }
+  });
+  
