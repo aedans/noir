d92c9812b9118c40249d028da044a479f5330720
aedans <aedan.smith@outlook.com>
2023-02-19 23:44:19 -0800

    Fix socket persistance

--- server/queue/Casual.ts
+++ server/queue/Casual.ts
@@ -7,11 +7,16 @@ export default class Casual implements Queue {
   sockets: Socket[] = [];
 
   async push(socket: Socket): Promise<void> {
+    socket.on("disconnect", () => {
+      this.sockets = this.sockets.filter(s => s != socket);
+    });
+
     this.sockets.push(socket);
 
     if (this.sockets.length >= 2) {
-      await createGame([new SocketPlayer(this.sockets[0]), new SocketPlayer(this.sockets[1])]);
+      const [first, second] = this.sockets; 
       this.sockets = this.sockets.slice(2);
+      await createGame([new SocketPlayer(first), new SocketPlayer(second)]);
     }
   }
 }
