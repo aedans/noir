3cb7dbfb5a1fd7af62863c8fa31e34d9e49cde12
Alex Hutchinson <alexh747@gmail.com>
2023-03-10 03:46:33 +0000

    oh god green nerfs

--- public/cards/Charismatic Industrialist.js
+++ public/cards/Charismatic Industrialist.js
@@ -2,14 +2,27 @@
 /** @type {import("../../common/card").PartialCardInfoComputation} */
 exports.card = (util, cache, game, card) => ({
   type: "agent",
-  text: "Activate this: steal an agent from your opponent, putting it onto your board revealed.",
+  text: "Activate this: steal an agent from your opponent, putting it onto your board revealed. It gains Disloyal.",
   cost: { money: 30, agents: 1 },
   colors: ["green"],
   keywords: ["vip"],
   activateTargets: {
     zones: ["deck", "board"],
+    types: ["agent"],
   },
   activate: function* (target) {
     yield* util.stealCard(cache, game, card, { target, zone: "board" });
+    yield* util.modifyCard(cache, game, card, {
+      target,
+      modifier: {
+        card,
+        name: "disloyal",
+      },
+    });
+  },
+  modifiers: {
+    disloyal: (info, modifier, card) => ({
+      keywords: ["disloyal"],
+    }),
   },
 });
