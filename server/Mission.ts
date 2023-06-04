60c9ad998ee6825361d38ca72a958bdf9d1a3720
aedans <aedan.smith@outlook.com>
2023-06-03 21:36:13 -0700

    Whoops

--- server/Mission.ts
+++ server/Mission.ts
@@ -1,9 +1,9 @@
 import { PlayerId } from "../common/gameSlice";
 import Player from "./Player";
-import Dapril from "./solo/Daphril";
+import Daphril from "./solo/Daphril";
 
 export type MissionName = "daphril";
 
 export const missions: { [T in MissionName]: (playerId: PlayerId) => Player } = {
-  daphril: (playerId) => new Dapril(playerId),
+  daphril: (playerId) => new Daphril(playerId),
 }
