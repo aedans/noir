8f5937729bc71f553877738a4bc4e27168d844d2
aedans <aedan.smith@outlook.com>
2023-06-20 11:52:05 -0700

    Alex pls2

--- public/cards/Crimson Night's Beckon.js
+++ public/cards/Crimson Night's Beckon.js
@@ -1,24 +1,22 @@
 // @ts-check
 /** @type {import("../../common/card").PartialCardInfoComputation} */
 exports.card = (util, cache, game, card) => ({
-    text: "Remove one of your opponent's agents. Return this to your deck next turn with $2 added to its cost. ",
-    type: "operation",
-    cost: {
-        money:
-          card.props.Played
-            ? 11 + 2* card.props.Played
-            : 11 ,
-        agents: 1,
-      },
-    targets: {
-      types: ["agent", "operation"],
-    },
-    play: function* (target) {
-      if(card.props.Played == isNull){
-        yield* util.setProp(cache, game, card, {target: card, name: "Played", value: 1})}else{
-            yield* util.setProp(cache, game, card, {target: card, name: "Played", value: card.props.Played + 1}),
+  text: "Remove one of your opponent's agents. Return this to your deck next turn with $2 added to its cost. ",
+  type: "operation",
+  cost: {
+    money: card.props.Played ? 11 + 2 * card.props.Played : 11,
+    agents: 1,
+  },
+  targets: {
+    types: ["agent", "operation"],
+  },
+  play: function* (target) {
+    if (card.props.Played != null) {
+      yield* util.setProp(cache, game, card, { target: card, name: "Played", value: 1 });
+    } else {
+      yield* util.setProp(cache, game, card, { target: card, name: "Played", value: card.props.Played + 1 });
       yield* util.removeCard(cache, game, card, { target });
-      yield* util.bounceCard(cache,game,card, {target: card})
-    }},
-  });
-  
+      yield* util.bounceCard(cache, game, card, { target: card });
+    }
+  },
+});
