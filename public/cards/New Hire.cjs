4fc83b2aacfa3d23d1feb347ef0576762f91e7f1
aedans <aedan.smith@outlook.com>
2023-06-24 12:34:24 -0700

    Update new hire

--- public/cards/New Hire.cjs
+++ public/cards/New Hire.cjs
@@ -3,12 +3,28 @@
 exports.card = (util, cache, game, card) => ({
   type: "agent",
   cost: { money: 6 },
-  text: "When this is removed, gain $1.",
+  text: "When this is removed, add a purple Random Citizen to your deck.",
   colors: ["purple"],
   onRemove: function* () {
-    yield* util.addMoney(cache, game, card, {
-      player: util.findCard(game, card).player,
-      money: 1,
+    const target = util.cid();
+    yield* util.addCard(cache, game, card, {
+      target,
+      name: "Random Citizen",
+      player: util.self(game, card),
+      zone: "deck",
     });
+
+    yield* util.modifyCard(cache, game, card, {
+      target,
+      modifier: {
+        name: "purple",
+        card,
+      },
+    });
+  },
+  modifiers: {
+    purple: (info) => ({
+      colors: ["purple"],
+    }),
   },
 });
