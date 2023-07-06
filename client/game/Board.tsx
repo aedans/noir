d81bbfdf5ea166ef7633ee754c45ccef43f9994d
aedans <aedan.smith@outlook.com>
2023-07-06 10:43:34 -0700

    Scale board

--- client/game/Board.tsx
+++ client/game/Board.tsx
@@ -11,14 +11,16 @@ export default function Board() {
   const player = useContext(PlayerContext);
   const board = useClientSelector((state) => state.game.current.players[player].board);
   const cards = useCardInfoList(board, [board]);
-
-  const x = (targetResolution.width - cards.length * (cardWidth + 10)) / 2 + cardWidth / 2;
+  
+  const scale = Math.min(1, 8 / cards.length);
+  const scaledCardWidth = scale * cardWidth;
+  const x = (targetResolution.width - cards.length * (scaledCardWidth + 10)) / 2 + scaledCardWidth / 2;
   const y = targetResolution.height * (2 / 4) + cardHeight / 2;
 
   return (
     <>
       {cards.map(({ state, info }, i) => (
-        <BoardCard state={state} info={info} key={state.id} x={x + i * (cardWidth + 10)} y={y} />
+        <BoardCard scale={scale} state={state} info={info} key={state.id} x={x + i * (scaledCardWidth + 10)} y={y} />
       ))}
     </>
   );
