f6e895eebcba763d8b064a48515985acca059808
Alex Hutchinson <119699792+SalubriousSnail@users.noreply.github.com>
2023-07-08 03:22:28 +0000

    cagey ehe

--- public/cards/Cagey Questioning.cjs
+++ public/cards/Cagey Questioning.cjs
@@ -1,8 +1,9 @@
 // @ts-check
 /** @type {import("../../common/card").PartialCardInfoComputation} */
 exports.card = (util, cache, game, card) => ({
-    text: "Reveal two cards.",
+    text: "Reveal two cards in your opponent's deck. After one is played, triple the cost of the other.",
     type: "operation",
+    colors: ["blue"],
     cost: { money: 2, agents: 1 },
     play: function* () {
       const revealed = yield* util.revealRandom(cache, game, card, 2, {
