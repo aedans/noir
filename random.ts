9d197c85ba0680eeeafab6761ff475a01d996eb9
aedans <aedan.smith@outlook.com>
2023-08-04 22:51:24 -0700

    Multithread random

--- random.ts
+++ random.ts
@@ -1,12 +1,39 @@
+import { Winner } from "./common/gameSlice.js";
 import { createGame } from "./server/game.js";
 import Random from "./server/solo/Random.js";
+import { isMainThread, Worker, parentPort } from "worker_threads";
 
-for (let i = 0; i < 100; i++) {
-  const players: [Random, Random] = [new Random(0, 1), new Random(1, 1)];
-  players[0].timeout = false;
-  players[1].timeout = false;
+if (!isMainThread) {
+  parentPort?.on('message', () => {
+    const players: [Random, Random] = [new Random(0, 1), new Random(1, 1)];
+    players[0].timeout = false;
+    players[1].timeout = false;
+  
+    createGame(players, (winner) => {
+      parentPort?.postMessage(winner)
+    });  
+  })
+} else {
+  async function runSet(number: number) {
+    console.log(`Round ${number + 1}`);
+    const promises: Promise<void>[] = [];
 
-  createGame(players, (winnner) => {
-    console.log(`Game ${i}: ${winnner}`);
-  });
+    for (let i = 0; i < 8; i++) {
+      promises.push(new Promise((resolve) => {
+        const worker = new Worker("./random.js");
+        worker.postMessage(undefined);
+        worker.on("message", (winner: Winner) => {
+          console.log(`Game ${i} winner: ${winner}`)
+          worker.terminate();
+          resolve();
+        });
+      }));
+    }
+
+    await Promise.all(promises);
+  }
+  
+  for (let i = 0;; i++) {
+    await runSet(i);
+  }
 }
