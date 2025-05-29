ccada8e9375df47c5022ae84479d9bf99ce264d3
aedans <aedan.smith@gmail.com>
2025-05-29 01:59:12 -0700

    Update story

--- stories/Card.stories.tsx
+++ stories/Card.stories.tsx
@@ -14,8 +14,12 @@ export default {
 export const Default = {
   render: (args, ctx) => {
     return new Root(ctx.parameters.pixi, (root) => {
-      const state = defaultCardState("", "");
-      const info = fillPartialCardInfo({});
+      const state = defaultCardState("Random Citizen", "");
+      const info = fillPartialCardInfo({
+        type: "agent",
+        cost: { money: 3 },
+        keywords: [["disloyal"]],
+      });
       createRoot(root.view).render(
         <AppProvider value={ctx.parameters.pixi.app}>
           <Container x={cardWidth / 2} y={cardHeight / 2}>
