f873cf100c1613e2e9accb404b8c3f98d195d63f
aedans <aedan.smith@gmail.com>
2025-05-29 23:49:32 -0700

    Add storybook button

--- client/menu/Menu.tsx
+++ client/menu/Menu.tsx
@@ -13,46 +13,53 @@ export default function Menu() {
     setLocation("/queue/Tutorial/");
   }
 
-  let y = -200;
+  let y = -400;
 
   return (
     <Container x={targetResolution.width / 2} y={targetResolution.height / 2 - 200}>
       <Button
         text="Play"
-        y={y += 200}
+        y={(y += 200)}
         pointerdown={() => {
           setLocation("/play/");
         }}
       />
       <Button
         text="Decks"
-        y={y += 200}
+        y={(y += 200)}
         pointerdown={() => {
           setLocation("/decks/");
         }}
       />
       <Button
+        text={auth.token == null ? "Login" : "Logout"}
+        y={(y += 200)}
+        pointerdown={() => {
+          const endpoint = auth.token == null ? "login" : "logout";
+          window.location.href = `${serverOrigin}/${endpoint}?returnTo=${window.location.host}`;
+        }}
+      />
+      <Button
         text="Replays"
-        y={y += 200}
+        y={(y += 200)}
         pointerdown={() => {
           setLocation("/replays/");
         }}
       />
       <Button
         text="Tutorial"
-        y={y += 200}
+        y={(y += 200)}
         pointerdown={() => {
           setLocation("/queue/Tutorial/");
         }}
       />
       <Button
-        text={auth.token == null ? "Login" : "Logout"}
-        y={y += 200}
+        text="Storybook"
+        y={(y += 200)}
         pointerdown={() => {
-          const endpoint = auth.token == null ? "login" : "logout";
-          window.location.href = `${serverOrigin}/${endpoint}?returnTo=${window.location.host}`;
+          window.location.href = "https://aedans.github.io/noir/";
         }}
-      />
+      ></Button>
     </Container>
   );
 }
